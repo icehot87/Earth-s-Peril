c5b7b87d260ecef01e2601ba5afb404ee68b1308
Kishore Veni <kishore.sv87@gmail.com>
2020-06-19 21:37:18 -0400

    Fixed score calculation bugs

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+
+    Level level;
+    GameSession gameStatus;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameSession>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        gameStatus.ScoreCalculator();
+        level.BlockDestroyed();
     }
 }
---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class GameSession : MonoBehaviour
+{
+    [Range(0.1f,10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int gameScore = 0;
+    [SerializeField] int pointsPerBlockDestroyed = 13;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if(gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = gameScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void ScoreCalculator()
+    {
+        gameScore += pointsPerBlockDestroyed;
+        scoreText.text = gameScore.ToString();
+    }
+    
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
